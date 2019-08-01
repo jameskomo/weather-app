2515f6cd66d94b3798d74e8d05c80a75889cee00
jameskomo <james.komoh@gmail.com>
2019-08-01 13:36:39 +0300

    const bodyParser = require('body-parser'); used to make the app use the installed body-parser middleware and app.use(bodyParser.urlencoded({ extended: true })); used to make the application use the middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //jshint esversion: 6
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 
