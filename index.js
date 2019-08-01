432b4a8f081dbe1261b147e3d19bad3723e57dcb
jameskomo <james.komoh@gmail.com>
2019-08-01 13:12:03 +0300

    Index.js get function res.send replaced with res.render to allow rendering of index.ejs in views folder

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.send('This is a Weather App!')
+  res.render('index');
 })
 
 app.listen(3000, function () {
