7d3cf5ad140dc9d7181ebd0819a70e10058db9ba
jameskomo <james.komoh@gmail.com>
2019-08-01 13:33:08 +0300

    Post route created to allow access of input from user and body-parser middleware installed that allows us to make use of the key-value pairs stored on the req-body object.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 //jshint esversion: 6
 
-const express = require('express')
+const express = require('express');
 const app = express()
-app.set('view engine', 'ejs')
+
 app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
 
 app.get('/', function (req, res) {
   res.render('index');
 })
+
+
+app.post('/', function (req, res) {
+    res.render('index');
+  })
+  
 
 app.listen(3000, function () {
   console.log('Weather app listening on port 3000!')
